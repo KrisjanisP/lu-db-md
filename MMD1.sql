193bfe11e3c913158ef47f93f5fe02fd9cce4f66
KrisjanisP <krisjanispetrucena@gmail.com>
2023-03-16 16:30:24 +0200

    finish

--- MMD1.sql
+++ MMD1.sql
@@ -47,27 +47,15 @@ SELECT tasks.id, users.id FROM users CROSS JOIN tasks;
 SELECT * FROM submissions WHERE user_id = 1 AND task_id <> 3 AND subm_time IS NOT NULL
                           ORDER BY task_id DESC -- selects all user 1 submission except for task 3 and orders by task id descending
 
-SELECT * FROM users FULL JOIN submissions s on users.id = s.user_idCREATE TABLE users (
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	username VARCHAR(64) NOT NULL UNIQUE,
-	password VARCHAR(128) NOT NULL,
-	first_name VARCHAR(64),
-	last_name VARCHAR(64),
-	email VARCHAR(128) NOT NULL UNIQUE
-);
+SELECT users.username, tasks.name FROM users
+    JOIN submissions on users.id = submissions.user_id
+    JOIN tasks on tasks.id = submissions.task_id
+ORDER BY users.username -- shows all users and the respective tasks that they have attempted to solve
 
-CREATE TABLE submissions (
-	id INT IDENTITY(1,1) PRIMARY KEY,
-	subm_time DATETIME,
-	user_id INT NOT NULL,
-	task_id INT NOT NULL,
-	language VARCHAR(20),
-	source_code VARCHAR(10000),
-	CONSTRAINT fk_submission_user FOREIGN KEY (user_id)
-	REFERENCES users(id)
-);
+DELETE FROM submissions
+DELETE FROM tasks
+DELETE FROM users
 
-CREATE TABLE task (
-	id [INT] [PRIMARY KEY] [AUTO INCREMENT]
-	checker [VARCHAR]
-);
+DROP TABLE submissions
+DROP TABLE tasks
+DROP TABLE users
